560e62652de4bcb011b5ec5bb94e80296d0d4085
maxmakasinn <max.makasinn@gmail.com>
2021-09-23 22:27:22 +0300

    collect tiitle, price and instock info of books

--- basic_scrap.py
+++ basic_scrap.py
@@ -3,22 +3,18 @@ from bs4 import BeautifulSoup
 
 book_list = []
 
-for x in range(1,51):
+for x in range(1, 51):
     print(x)
-    url = f'https://books.toscrape.com/catalogue/page-{x}.html'
-    r =  requests.get(url)
+    url = f"https://books.toscrape.com/catalogue/page-{x}.html"
+    r = requests.get(url)
 
-    soup = BeautifulSoup(r.text, 'html.parser')
-    article = soup.find_all('article', class_ = 'product_pod')
+    soup = BeautifulSoup(r.text, "html.parser")
+    article = soup.find_all("article", class_="product_pod")
 
     for book in article:
-        title = book.find_all('a')[1]['title']
-        price = book.find('p', class_ = 'price_color').text[2:]
-        instock = book.find('p', class_ = 'instock availability').text.strip()
-        books = {
-            "title": title,
-            'price': price,
-            'instock': instock
-        }
+        title = book.find_all("a")[1]["title"]
+        price = book.find("p", class_="price_color").text[2:]
+        instock = book.find("p", class_="instock availability").text.strip()
+        books = {"title": title, "price": price, "instock": instock}
         book_list.append(books)
 print(len(book_list))

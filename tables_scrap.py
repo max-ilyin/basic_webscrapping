b479a10faec05a3f6dd6f624dfe372f70ee2b216
maxmakasinn <max.makasinn@gmail.com>
2021-09-25 21:34:43 +0300

    collected data to files

--- tables_scrap.py
+++ tables_scrap.py
@@ -1,26 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
-#url = "https://www.skysports.com/premier-league-table"
+# url = "https://www.skysports.com/premier-league-table"
+
 
 def get_league_table(url):
     r = requests.get(url)
-    soup = BeautifulSoup(r.text, 'html.parser')
+    soup = BeautifulSoup(r.text, "html.parser")
     tablelist = []
-    league_table = soup.find('table', class_ = "standing-table__table")
-    for team in league_table.find_all('tbody'):
-        rows = team.find_all('tr')
+    league_table = soup.find("table", class_="standing-table__table")
+    for team in league_table.find_all("tbody"):
+        rows = team.find_all("tr")
         for row in rows:
-            pl_team = row.find('td', class_ = 'standing-table__cell standing-table__cell--name').text.strip()
-            pl_points = row.find_all('td', class_ = 'standing-table__cell')[9].text
+            pl_team = row.find(
+                "td", class_="standing-table__cell standing-table__cell--name"
+            ).text.strip()
+            pl_points = row.find_all("td", class_="standing-table__cell")[9].text
             # print(pl_team, pl_points)
-            teamleague = {
-                'name': pl_team,
-                'points': pl_points
-            }
+            teamleague = {"name": pl_team, "points": pl_points}
             tablelist.append(teamleague)
     return tablelist
 
+
 data = get_league_table("https://www.skysports.com/premier-league-table")
 
-print(data)
+df = pd.DataFrame(data)
+df.to_csv("leagetable.csv")
+df.to_excel("leagetable.xlsx")
+print("done")

77f50a457db2a1087201aadb6d284db150520ab3
maxmakasinn <max.makasinn@gmail.com>
2021-09-25 21:22:26 +0300

    made function to collect data in to a list with information

--- tables_scrap.py
+++ tables_scrap.py
@@ -1,17 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.skysports.com/premier-league-table"
+#url = "https://www.skysports.com/premier-league-table"
 
-r = requests.get(url)
+def get_league_table(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    tablelist = []
+    league_table = soup.find('table', class_ = "standing-table__table")
+    for team in league_table.find_all('tbody'):
+        rows = team.find_all('tr')
+        for row in rows:
+            pl_team = row.find('td', class_ = 'standing-table__cell standing-table__cell--name').text.strip()
+            pl_points = row.find_all('td', class_ = 'standing-table__cell')[9].text
+            # print(pl_team, pl_points)
+            teamleague = {
+                'name': pl_team,
+                'points': pl_points
+            }
+            tablelist.append(teamleague)
+    return tablelist
 
-soup = BeautifulSoup(r.text, 'html.parser')
+data = get_league_table("https://www.skysports.com/premier-league-table")
 
-league_table = soup.find('table', class_ = "standing-table__table")
-
-for team in league_table.find_all('tbody'):
-    rows = team.find_all('tr')
-    for row in rows:
-        pl_team = row.find('td', class_ = 'standing-table__cell standing-table__cell--name').text.strip()
-        pl_points = row.find_all('td', class_ = 'standing-table__cell')[9].text
-        print(pl_team, pl_points)
+print(data)

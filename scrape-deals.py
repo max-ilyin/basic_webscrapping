fe075c5458787e44c2f864cd3fbc01b3ba5d2cdd
maxmakasinn <max.makasinn@gmail.com>
2021-09-29 20:16:07 +0300

    colect prices and make function to add this prices

--- scrape-deals.py
+++ scrape-deals.py
@@ -11,11 +11,18 @@ def get_price(url):
     options.add_argument("--headless")
     driver = webdriver.Firefox(options=options, executable_path=DRIVER_PATH)
     driver.get(url)
-    print(driver.page_source)
+    # print(driver.page_source)
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     price = soup.find('span', {'itemprop': 'price'})['content']
     driver.quit()
     return price
 
-print(get_price('https://www.scan.co.uk/products/intel-core-i7-10700kf-s-1200-comet-lake-8-cores-16-threads-38ghz-51ghz-turbo-16mb-cache'))
+def add_prices(p1, p2, p3):
+    total = (float(p1) + float(p2) + float(p3))
+    return total
 
+cpu = get_price('https://www.scan.co.uk/products/intel-core-i7-10700kf-s-1200-comet-lake-8-cores-16-threads-38ghz-51ghz-turbo-16mb-cache')
+motherboard = get_price('https://www.scan.co.uk/products/msi-mag-tomahawk-intel-z490-s-1200-ddr4-sata3-2x-m2-crossfire-25gbe-usb-32-gen1-atx')
+ram = get_price('https://www.scan.co.uk/products/16gb-(1x16gb)-corsair-ddr4-vengeance-lpx-black-pc4-19200-(2400)-non-ecc-unbuffered-cas-16-16-16-39-x')
+
+print(add_prices(cpu, motherboard, ram))
